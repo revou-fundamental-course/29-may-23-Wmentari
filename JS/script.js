4763aca92f20b3ce6715839d4daf90d455355f19
Wmentari <widimentari05@gmail.com>
2023-06-08 20:30:38 +0700

    feat: add home html, and change title

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+var name = prompt("Please enter your name");
+        document.getElementById("nama").innerText = name;
+
+        
+        function validateForm() {
+            var name = document.forms["message-form"]["full-name"].value;
+            var date = document.forms["message-form"]["birth-date"].value;
+            var gender = document.forms["message-form"]["gender"].value;
+            var messages = document.forms["message-form"]["messages"].value;
+
+            if (name == "" || date == "" || gender == "" || messages == "") {
+              alert("Input cannot be empty!");
+              return false;
+            }
+
+            document.getElementById("sender-full-name").innerText = name;
+            document.getElementById("sender-birth-date").innerText = date;
+            document.getElementById("sender-gender").innerText = gender;
+            document.getElementById("sender-messages").innerText = messages;
+            return false;
+          }
+
+          
